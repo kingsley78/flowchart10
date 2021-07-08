9e8fc6899e02c66bdf1940a4b9636e638efb5bf3
kingsley78 <85633140+kingsley78@users.noreply.github.com>
2021-07-08 08:15:33 +0100

    Add files via upload

---
+++ flow10.py
@@ -0,0 +1,11 @@
+#flowchart10
+while True:
+    print("who are you")
+    name = input("enter")
+    if name !="joe":
+        continue
+    print("hello joe,what is the password?(it is a fish)")
+    password = input("what is the password ")
+    if password == "swordfish":
+        break
+    print("access granted")
